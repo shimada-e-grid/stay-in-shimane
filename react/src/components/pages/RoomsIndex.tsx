b46897dab4d36978efb07568870a30ee1182f80f
shimada <shimada@e-grid.co.jp>
2022-09-09 22:08:34 +0900

    add axios hooks

--- react/src/components/pages/RoomsIndex.tsx
+++ react/src/components/pages/RoomsIndex.tsx
@@ -1,4 +1,5 @@
 import { Button, TextField } from '../'
+import useAxios from '../../hooks/useAxios'
 
 const handleOnClickShow = () => {
   window.location.href = '/rooms/1'
@@ -10,7 +11,16 @@ const handleOnClickSearch = () => {
 
 }
 
+interface RoomResponse {
+  name: string
+}
+
 export const RoomsIndex: React.FC = () => {
+  const [{ response, loading }] = useAxios<{ data: RoomResponse[] }>({
+    url: `api/v1/rooms?prefecture=32`,
+    method: 'get'
+  });
+
   return (
     <>
       <p>物件一覧</p>
@@ -18,6 +28,7 @@ export const RoomsIndex: React.FC = () => {
       <Button onClick={handleOnClickTop}>トップ</Button>
       <TextField />
       <Button onClick={handleOnClickSearch}>検索</Button>
+
     </>
   )
 }
