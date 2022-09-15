80dfb814bdfce709129f444d85fdc3fb3646dea4
aoi415 <aoi415desu@gmail.com>
2022-09-15 16:26:58 +0900

    物件削除時、紐づく物件カレンダーテーブルのデータを削除する

--- rails/app/models/room.rb
+++ rails/app/models/room.rb
@@ -4,7 +4,7 @@ class Room < ApplicationRecord
   belongs_to :user
 
   has_many :room_images
-  has_many :room_calendars
+  has_many :room_calendars, dependent: :destroy
   has_many :holidays
   has_many :reserveds
 
