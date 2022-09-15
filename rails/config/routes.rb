29173fcdcb9bf6c3d56f95fe563938fac86f3c56
masatoOkuni <zhengdengdaguo@gmail.com>
2022-09-15 11:19:48 +0900

    ホスト側物件情報作成

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
       mount_devise_token_auth_for 'User', at: 'auth'
       resources :rooms
       resource :status, only: :show
+      namespace :host do
+        resources :rooms
+      end
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
