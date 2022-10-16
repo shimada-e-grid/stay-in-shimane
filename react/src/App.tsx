5adbdad1e04f3e6dd29dc28d3b326b39bae7ee0c
Yu Birel <dev.birel.z32.f1@gmail.com>
2022-10-16 16:41:07 +0900

    全体にpaddingを追加

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -4,7 +4,7 @@ import { Providers } from "./components/templates/Provider"
 export const App: React.FC = () => {
   return (
     <Providers>
-      <div className="App">
+      <div className="App px-8 py-4">
         <header className="App-header">
         </header>
         <Layout />
